58b660abf55bea33c799924ac51faabc08ee8942
Kevin Kariuki <vingentz@gmail.com>
2023-04-28 21:45:28 +0300

    Ammended main.h file

--- main.h
+++ main.h
@@ -7,11 +7,17 @@
 #include <unistd.h>
 
 /**
- * _strcpy - copies the string pointed to by src, including the terminating null byte (\0),
- * to the buffer pointed to by dest
+ * _stringcopy - copies the string pointed to by src,
+ * including the terminating null byte (\0) to buffer pointed to by dest
  * @dest: copy source to this buffer
  * @src: this is the source to copy
  */
 void string_copy(char *dest, const char *src);
+
+/**
+ * execmd - execute command
+ * @argv: arguement to take in
+ */
+void execmd(char **argv);
 
 #endif
